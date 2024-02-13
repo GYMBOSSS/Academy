ac4ca2ae8eddcc06ee05c2d9da2a7257b8657431
Alexandr <116194455+GYMBOSSS@users.noreply.github.com>
2024-02-13 18:10:19 +0400

    Added homework 'Конструктор и деструктор, студенты и точки'

--- Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23.cpp
+++ Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class Student
 {
+public:
 	string name;
 	string birthday;
 	string group;
@@ -31,7 +32,78 @@ class Student
 	vector<int> getmarks() { return marks; }
 };
 
-int main()
-{
-   
+void generateGraph(Student student) {
+    vector<int> marks = student.getmarks();
+
+    int maxMark = *max_element(marks.begin(), marks.end());
+
+    vector<vector<char>> graph(maxMark + 1);
+    for (int i = 0; i <= maxMark; i++) {
+        graph[i].resize(marks.size(), ' ');
+    }
+
+    for (int i = 0; i < marks.size(); i++) {
+        for (int j = 0; j < marks[i]; j++) {
+            graph[j][i] = '*';
+        }
+    }
+
+    for (int i = maxMark; i >= 0; i--) {
+        for (int j = 0; j < marks.size(); j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    vector<Student> students;
+
+    string input;
+
+    while (true) {
+        cout << "Enter input (type 'end' to stop or 'new' to create new student): ";
+        cin >> input;
+
+        if (input == "end") {
+            break; // Exit the loop if the user entered "end"
+        }
+        else if (input == "new") {
+            string name;
+            string birthday;
+            string group;
+            vector<int> marksoo{ 5,5,5,5,5,5,5 };
+            cin >> name;
+            cin >> birthday;
+            cin >> group;
+
+            Student student(name, birthday, group);
+            students.push_back(student);
+        }
+    }
+
+    // Get the name of the student whose graph you want to generate
+    cout << "Enter the name of the student whose graph you want to generate: ";
+    string name;
+    cin >> name;
+
+    // Find the student in the list of students
+    Student* studentPtr = nullptr;
+    for (Student& student : students) {
+        if (student.getname() == name) {
+            studentPtr = &student;
+            break;
+        }
+    }
+
+    // If the student was found, generate the graph
+    if (studentPtr != nullptr) {
+        generateGraph(*studentPtr);
+    }
+    else {
+        cout << "Student not found." << endl;
+    }
+
+    return 0;
 }
