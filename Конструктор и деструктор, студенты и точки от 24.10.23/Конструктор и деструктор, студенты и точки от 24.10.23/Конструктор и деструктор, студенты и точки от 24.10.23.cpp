439a8fb8d084c736bc3d35a1280a715cc59fd6fe
Alexandr <116194455+GYMBOSSS@users.noreply.github.com>
2024-02-13 15:49:58 +0400

    Added homework 'Конструктор и деструктор, студенты и точки'

---
+++ Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23/Конструктор и деструктор, студенты и точки от 24.10.23.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Student
+{
+	string name;
+	string birthday;
+	string group;
+	vector<int> marks;
+
+	Student(string nameP, string birthdayP, string groupP) : name(nameP), birthday(birthdayP), group(groupP) {};
+	
+	void setname(string nameP) { name = nameP; }
+	void setbirthday(string birthdayP) { birthday = birthdayP; }
+	void setgroup(string groupP) { group = groupP; }
+
+	void setmark(int mark)
+	{
+		marks.push_back(mark);
+		for (int mark : marks) { cout << mark; }
+	}
+	void setmarks(vector<int> newmarks) { marks = newmarks; }
+
+	string getname() { return name; }
+	string getbirthday() { return birthday; }
+	string getgroup() { return group; }
+
+	int getmark(int mark){ marks.back();}
+	vector<int> getmarks() { return marks; }
+};
+
+int main()
+{
+   
+}
